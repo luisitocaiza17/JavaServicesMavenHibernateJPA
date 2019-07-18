52705665cd9c12af21103aed0ca23f88e2d6c521
luis caiza <luisito.caiza17@gmail.com>
2019-07-18 16:32:07 -0500

    coneccion POstgress

--- target/generated-sources/annotations/com/mycompany/Entities/Persona_.java
+++ target/generated-sources/annotations/com/mycompany/Entities/Persona_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-17T07:53:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-07-18T15:12:01")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
